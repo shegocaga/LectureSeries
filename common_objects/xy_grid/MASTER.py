6f15df81171d2ee5e03c162b0c1c17a721fe2b33
zachandfox <zachandfox@gmail.com>
2019-02-09 16:51:13 -0600

    Add code for horizontal bars, and some generation generalization

--- common_objects/xy_grid/MASTER.py
+++ common_objects/xy_grid/MASTER.py
@@ -63,11 +63,21 @@ bevel_plot = bpy.context.scene.objects["extrude_path_object"]
 ### Add plot as a path of extrusion (for drawing of plot animation) ###
 #######################################################################
 
-d_tick = 6 #Time displacement between ticks
+d_tick = 1.5 #Time displacement between ticks
 d_grow = 6 #Time needed to grow line
+left_bound = -10
+right_bound = 10
+lower_bound = -10
+upper_bound = 10
+x_step = 1
+y_step = 1
 
-for iter in range(-10,11):
-    plot = [(iter,-10,0),(iter,10,0)]
+n_xbars = int((right_bound-left_bound)/x_step+1)
+n_ybars = int((upper_bound-lower_bound)/y_step+1)
+
+# Plot Vertical 
+for iter in range(n_xbars):
+    plot = [(left_bound+iter*x_step,-10,0),(left_bound+iter*x_step,10,0)]
 
     plot_path_data = bpy.data.curves.new('plot_path'+str(iter), type='CURVE')
     plot_path_data.dimensions = '3D'
@@ -110,10 +120,63 @@ for iter in range(-10,11):
     ###############
     ### Animate ###
     ###############
-    bpy.context.scene.frame_current = (10+iter)*d_tick
+    bpy.context.scene.frame_current = (iter)*d_tick
     curve.bevel_factor_end = 0
-    curve.keyframe_insert("bevel_factor_end", frame=(10+iter)*d_tick)
+    curve.keyframe_insert("bevel_factor_end", frame=(iter)*d_tick)
 
-    bpy.context.scene.frame_current = (10+iter)*d_tick + d_grow
+    bpy.context.scene.frame_current = (iter)*d_tick + d_grow
     curve.bevel_factor_end = 1
-    curve.keyframe_insert("bevel_factor_end", frame=(10+iter)*d_tick + d_grow)
+    curve.keyframe_insert("bevel_factor_end", frame=(iter)*d_tick + d_grow)
+
+# Plot Horizontal 
+for iter in range(n_ybars):
+    plot = [(-10,lower_bound + iter*y_step,0),(10,lower_bound + iter*y_step,0)]
+
+    plot_path_horz_data = bpy.data.curves.new('plot_path_horz'+str(iter), type='CURVE')
+    plot_path_horz_data.dimensions = '3D'
+    plot_path_horz_data.resolution_u = 2
+
+    # map coords to spline 
+    polyline = plot_path_horz_data.splines.new('POLY')
+    polyline.points.add(len(plot)-1)
+    for i, pt in enumerate(plot):
+        x,y,z = pt
+        polyline.points[i].co = (x, y, z, 1)
+
+    # create Object
+    curveOB = bpy.data.objects.new('plot_path_horz'+str(iter), plot_path_horz_data)
+
+    # attach to scene and validate context
+    scn = bpy.context.scene
+    scn.objects.link(curveOB)
+    scn.objects.active = curveOB
+    curveOB.select = True
+
+    ob = bpy.context.scene.objects["plot_path_horz"+str(iter)]
+    curve = ob.data
+
+    # Add color
+    color = colorsys.hsv_to_rgb(0,0,0)
+    activeObject = bpy.context.active_object
+    mat = bpy.data.materials.new(name="MaterialName") #set new material to variable
+    activeObject.data.materials.append(mat)
+    bpy.context.object.active_material.diffuse_color = color
+
+    #############################################################
+    ### Create bevel object from custom curve to bezier curve ###
+    #############################################################
+    #bpy.context.object.data.bevel_object = bpy.data.objects["cross_section"]
+    curve.bevel_object = bevel_plot
+    curve.use_fill_caps = True
+
+
+    ###############
+    ### Animate ###
+    ###############
+    bpy.context.scene.frame_current = (iter)*d_tick + n_xbars*d_grow
+    curve.bevel_factor_end = 0
+    curve.keyframe_insert("bevel_factor_end", frame=(10+iter)*d_tick + n_xbars*d_grow)
+
+    bpy.context.scene.frame_current = (iter)*d_tick + d_grow + n_xbars*d_grow
+    curve.bevel_factor_end = 1
+    curve.keyframe_insert("bevel_factor_end", frame=(10+iter)*d_tick + d_grow + n_xbars*d_grow)
